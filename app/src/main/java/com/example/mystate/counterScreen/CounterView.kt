6db64782d0e42ecbae513c96ec1a23e5b49bf381
Sergio Jimenez <serjicho@hotmail.com>
2023-11-20 11:48:33 +0100

    Añadiendo remeber conseguimos que jetpack compose recuerde el valor de una variable entre recomposiciones

--- app/src/main/java/com/example/mystate/counterScreen/CounterView.kt
+++ app/src/main/java/com/example/mystate/counterScreen/CounterView.kt
@@ -6,21 +6,31 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
 @Composable
 fun MyCounter() {
 
-    var count = 0;
+    //TODO Use of rememeber como habreis notado se estaba siguiendo el componenete
+    // y haciendo una recomposición. Pero el valir no se estaba incrementando, esto es debido
+    // a que con cada recomposición la variable count se esta creando una y otra vez.
+    // quedando atascada en 0, para evitar esto se utiliza el remember.
+    // De esta forma compose no solo sabrá que tiene que redibujar el compoenente,
+    // también se encargará de mantener en la valor de la variable.
+
+    var count = remember { mutableStateOf(0) }
+
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     )
     {
-
-        Button(onClick = { count += 0 }) {
+        //TODO now we have to change to .value, a it is now an object not an Int
+        Button(onClick = { count.value = 0 }) {
             Text(text = "pulsame")
         }
         Text(text = "hesido puslado ${count}")
